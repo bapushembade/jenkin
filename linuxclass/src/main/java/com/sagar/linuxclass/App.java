cebea866d207fd162ab18db361a522adb6c93029
root <root@server1.example.com>
2018-05-02 15:25:58 +0530

    First Commit

---
+++ linuxclass/src/main/java/com/sagar/linuxclass/App.java
@@ -0,0 +1,13 @@
+package com.sagar.linuxclass;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
